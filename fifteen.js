5bcb30b7db69fe99bcf97afcb03a0dca22f232b0
Daniel Dawson <Daniel Dawson>
2016-10-25 21:02:34 -0500

    Allow for only movable tiles to highlight on hover

--- fifteen.js
+++ fifteen.js
@@ -9,7 +9,7 @@ $(document).ready(function(e) {
 	var $pieces = $("#puzzlearea > div");
 	
 	//Keep track of the position of the empty space
-	var empty_pos = ["300px", "300px"];
+	var empty_pos = ["297px", "300px"];
 	var count = 0;
 	
 	//Loop for positioning tiles and background image
@@ -21,24 +21,25 @@ $(document).ready(function(e) {
 		}
 		if(i>11)
 		{
-			$pieces[i].style.top = (300) + 'px';
+			$pieces[i].style.top = (297) + 'px';
 			$pieces[i].style.left = (count * 100) + 'px';
 			$pieces[i].style.backgroundPosition = '-' + count * 100 + 'px 200px';
 		}
 		else if(i > 7)
 		{
-			$pieces[i].style.top = (200) + 'px';
+			$pieces[i].style.top = (198) + 'px';
 			$pieces[i].style.left = (count * 100) + 'px';
 			$pieces[i].style.backgroundPosition = '-' + count * 100 + 'px 300px';
 		}
 		else if(i > 3)
 		{
-			$pieces[i].style.top = (100) + 'px';
+			$pieces[i].style.top = (99) + 'px';
 			$pieces[i].style.left = (count * 100) + 'px';
 			$pieces[i].style.backgroundPosition = '-' + count * 100 + 'px 400px';
 		}
 		else
 		{
+			$pieces[i].style.top = '0px';
 			$pieces[i].style.left = (count * 100) + 'px';
 			$pieces[i].style.backgroundPosition = '-' + count * 100 + 'px 500px';
 		}
@@ -54,4 +55,42 @@ $(document).ready(function(e) {
 		this.style.top = new_top;
 		this.style.left = new_left;
 		});
+		
+	//Test if tile can move
+	function canMove(){
+		if(this.style.top == (parseInt(empty_pos[0].slice(0, (this.style.top.length - 2)))) - 99 || this.style.top == (parseInt(empty_pos[0].slice(0, (this.style.top.length - 2)))) + 99)
+		{
+			if(this.style.left == (parseInt(empty_pos[1].slice(0, (this.style.top.length - 2)))) - 100 || this.style.left == (parseInt(empty_pos[1].slice(0, (this.style.top.length - 2)))) + 100)
+			{
+				return true;	
+			}	
+		}
+	}
+		
+	//Red background on hover	
+	$pieces.hover(function(e) 
+	{
+		if(this.style.top == empty_pos[0] ||  this.style.left == empty_pos[1])
+		
+		{
+			var x = parseInt(empty_pos[0].slice(0, (empty_pos[0].length -2)));
+			var y = parseInt(empty_pos[1].slice(0, (empty_pos[1].length -2)));
+			
+			if( (x +99) + "px" == this.style.top || (x - 99) + "px" == this.style.top)
+			{
+				$(this).css("border-color", "#F00");
+			}
+			
+			if( (y +100) + "px" == this.style.left || (y - 100) + "px" == this.style.left)
+			{
+				$(this).css("border-color", "#F00");
+			}
+			
+		}
+    }, function()
+	{
+		$(this).css("border-color", "#000");
+	});
+		
+	
 });
